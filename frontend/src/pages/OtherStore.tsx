80a042f33671f87baa3cdc5a0f9604f6fc9ef515
YenaLey <yena.e121@gmail.com>
2024-12-27 13:24:27 +0900

    Chore

--- frontend/src/pages/OtherStore.tsx
+++ frontend/src/pages/OtherStore.tsx
@@ -1,91 +1,92 @@
 import "@/styles/OtherStore.css";
-import {TbSnowflake} from "react-icons/tb";
-import {IoIosArrowRoundBack} from "react-icons/io";
-import Store from "@/components/Store/Store"
-import React, {useEffect, useState} from "react";
-import {FaSearch} from "react-icons/fa";
+import { TbSnowflake } from "react-icons/tb";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import Store from "@/components/Store/Store";
+import React, { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import 팥붕 from "@/assets/팥붕.png";
 import 슈붕 from "@/assets/슈붕.png";
 import 민초붕 from "@/assets/민초붕.png";
 import 고구마붕 from "@/assets/고구마붕.png";
-import '@iconify/react';
+import "@iconify/react";
 import { Icon } from "@iconify/react";
 
 type FishType = "팥붕" | "슈붕" | "민초붕" | "고구마붕";
-const fishImages : Record<FishType, string> = {
-    팥붕: 팥붕,
-    슈붕: 슈붕,
-    민초붕: 민초붕,
-    고구마붕: 고구마붕,
+const fishImages: Record<FishType, string> = {
+  팥붕: 팥붕,
+  슈붕: 슈붕,
+  민초붕: 민초붕,
+  고구마붕: 고구마붕,
 };
 
-
 export default function Start() {
+  const [storeData] = useState({
+    data: {
+      loginUser: "예나",
+      username: "예나",
+      choiceType: "fish2",
+      coin: 1500,
+      memoChoiceCount: {
+        fish1: 2, //팥붕
+        fish2: 2, //슈붕
+        fish3: 3, //민초붕
+        fish4: 2, //고구마붕
+      },
+    },
+  });
 
+  const [selectedFish, setSelectedFish] = useState("");
+  const [myFish, setMyFish] = useState<FishType>("팥붕");
 
+  useEffect(() => {
+    console.log(selectedFish); //선택한 붕어빵 종류 출력
+  });
 
-    const [storeData] = useState({
-        data: {
-            loginUser: "예나",
-            username: "예나",
-            choiceType: "fish2",
-            coin: 1500,
-            memoChoiceCount: {
-                fish1: 2, //팥붕
-                fish2: 2, //슈붕
-                fish3: 3, //민초붕
-                fish4: 2, //고구마붕
-            },
-        },
-    });
+  useEffect(() => {
+    switch (storeData.data.choiceType) {
+      case "fish2":
+        setMyFish("슈붕");
+        break;
+      case "fish3":
+        setMyFish("민초붕");
+        break;
+      case "fish4":
+        setMyFish("고구마붕");
+        break;
+      default:
+        setMyFish("팥붕");
+        break;
+    }
+  }, []);
 
-    const [selectedFish, setSelectedFish] = useState("");
-    const [myFish, setMyFish] = useState<FishType>("팥붕")
-
-    useEffect(() => {
-        console.log(selectedFish); //선택한 붕어빵 종류 출력
-    });
-
-    useEffect(()=>{
-        switch (storeData.data.choiceType) {
-            case "fish2":
-                setMyFish("슈붕")
-                break;
-            case "fish3":
-                setMyFish("민초붕")
-                break;
-            case "fish4":
-                setMyFish("고구마붕")
-                break;
-            default:
-                setMyFish("팥붕")
-                break;
-        }
-    }, []);
-
-    return (
-        <div className="otherstore-container">
-            <div className="back">
-                <IoIosArrowRoundBack/>
-            </div>
-            <div className="otherstore-top">
-                <TbSnowflake/>
-                <h1>{storeData.data.username}님의 붕어빵 가게</h1>
-            </div>
-            <div className="store-container">
-                <img src={fishImages[myFish]} alt="붕어빵 이미지" className="my-fish-image"/>
-                <Store
-                    username={storeData.data.username}
-                    memoChoiceCount={storeData.data.memoChoiceCount}
-                    setSelectedFish={setSelectedFish}
-                />
-                <div className="coin-box">
-                    <Icon icon="mingcute:currency-won-line" />
-                    <h4>{storeData.data.coin}</h4>
-                </div>
-
-            </div>
-            <button >모든 가게 <FaSearch/></button>
+  return (
+    <div className="otherstore-container">
+      <div className="back">
+        <IoIosArrowRoundBack />
+      </div>
+      <div className="otherstore-top">
+        <TbSnowflake />
+        <h1>{storeData.data.username}님의 붕어빵 가게</h1>
+      </div>
+      <div className="store-container">
+        <img
+          src={fishImages[myFish]}
+          alt="붕어빵 이미지"
+          className="my-fish-image"
+        />
+        <Store
+          username={storeData.data.username}
+          memoChoiceCount={storeData.data.memoChoiceCount}
+          setSelectedFish={setSelectedFish}
+        />
+        <div className="coin-box">
+          <Icon icon="mingcute:currency-won-line" />
+          <h4>{storeData.data.coin}</h4>
         </div>
-    );
+      </div>
+      <button>
+        모든 가게 <FaSearch />
+      </button>
+    </div>
+  );
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -7,200 +7,204 @@ import 민초붕 from "@/assets/민초붕.png";
 import 고구마붕 from "@/assets/고구마붕.png";
 
 export default function Register() {
-    // 상태 관리
-    const [formData, setFormData] = useState({
-        name: "",
-        studentID: "",
-        password: "",
-        confirmPassword: "",
-        choiceType: "fish1", // 기본 선택
-        topic: "",
-    });
-    const [errorMessage, setErrorMessage] = useState<string | null>(null);
-    const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  // 상태 관리
+  const [formData, setFormData] = useState({
+    name: "",
+    studentID: "",
+    password: "",
+    confirmPassword: "",
+    choiceType: "fish1", // 기본 선택
+    topic: "",
+  });
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
-    // 입력 변경 핸들러
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const { name, value } = e.target;
-        setFormData((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-    };
+  // 입력 변경 핸들러
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-    // 폼 제출 핸들러
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
+  // 폼 제출 핸들러
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-        // 비밀번호 확인
-        if (formData.password !== formData.confirmPassword) {
-            setErrorMessage("비밀번호가 일치하지 않습니다.");
-            return;
-        }
+    // 비밀번호 확인
+    if (formData.password !== formData.confirmPassword) {
+      setErrorMessage("비밀번호가 일치하지 않습니다.");
+      return;
+    }
 
-        try {
-            const response = await fetch("http://localhost:5001/register", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    name: formData.name,
-                    studentID: formData.studentID,
-                    password: formData.password,
-                    choiceType: formData.choiceType,
-                    topic: formData.topic,
-                }),
-            });
+    try {
+      const response = await fetch("http://localhost:5001/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: formData.name,
+          studentID: formData.studentID,
+          password: formData.password,
+          choiceType: formData.choiceType,
+          topic: formData.topic,
+        }),
+      });
 
-            if (response.ok) {
-                setSuccessMessage("붕어빵 가게 생성이 완료되었습니다!");
-                setErrorMessage(null);
+      if (response.ok) {
+        setSuccessMessage("붕어빵 가게 생성이 완료되었습니다!");
+        setErrorMessage(null);
 
-                // 폼 초기화
-                setFormData({
-                    name: "",
-                    studentID: "",
-                    password: "",
-                    confirmPassword: "",
-                    choiceType: "fish1",
-                    topic: "",
-                });
-            } else if (response.status === 400) {
-                const errorData = await response.json();
-                setErrorMessage(errorData.message || "잘못된 입력입니다.");
-            } else {
-                setErrorMessage("서버 오류가 발생했습니다. 다시 시도해주세요.");
-            }
-        } catch (error) {
-            setErrorMessage("네트워크 오류가 발생했습니다. 다시 시도해주세요.");
-        }
-    };
+        // 폼 초기화
+        setFormData({
+          name: "",
+          studentID: "",
+          password: "",
+          confirmPassword: "",
+          choiceType: "fish1",
+          topic: "",
+        });
+      } else if (response.status === 400) {
+        const errorData = await response.json();
+        setErrorMessage(errorData.message || "잘못된 입력입니다.");
+      } else {
+        setErrorMessage("서버 오류가 발생했습니다. 다시 시도해주세요.");
+      }
+    } catch (error) {
+      setErrorMessage("네트워크 오류가 발생했습니다. 다시 시도해주세요.");
+    }
+  };
 
-    return (
-        <div className={styles.registerContainer}>
-            <img src={mapSnow} alt="지도 아이콘" className={styles.mapSnow} />
-            <h1 className={styles.registerTitle}>내 붕어빵 가게 만들기</h1>
+  return (
+    <div className={styles.registerContainer}>
+      <img src={mapSnow} alt="지도 아이콘" className={styles.mapSnow} />
+      <h1 className={styles.registerTitle}>내 붕어빵 가게 만들기</h1>
 
-            {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-            {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+      {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
 
-            <form onSubmit={handleSubmit}>
-                <div className={styles.inputContainer}>
-                    <div className={styles.inputBox}>
-                        <input
-                            type="text"
-                            name="name"
-                            value={formData.name}
-                            onChange={handleChange}
-                            className={styles.inputField}
-                            placeholder="이름을 입력하세요."
-                            required
-                        />
-                    </div>
-                    <div className={styles.inputBox}>
-                        <input
-                            type="text"
-                            name="studentID"
-                            value={formData.studentID}
-                            onChange={handleChange}
-                            className={styles.inputField}
-                            placeholder="학번을 입력하세요."
-                            required
-                        />
-                    </div>
-                    <div className={styles.inputBox}>
-                        <input
-                            type="password"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            className={styles.inputField}
-                            placeholder="비밀번호를 설정하세요."
-                            required
-                        />
-                    </div>
-                    <div className={styles.inputBox}>
-                        <input
-                            type="password"
-                            name="confirmPassword"
-                            value={formData.confirmPassword}
-                            onChange={handleChange}
-                            className={styles.inputField}
-                            placeholder="다시 한번 입력하세요."
-                            required
-                        />
-                    </div>
-                </div>
-
-                <h2 className={styles.sectionTitle}>붕어빵을 선택하세요.</h2>
-                <div className={styles.fishSelection}>
-                    <label className={styles.fishItem}>
-                        <input
-                            type="radio"
-                            name="choiceType"
-                            value="fish1"
-                            checked={formData.choiceType === "fish1"}
-                            onChange={handleChange}
-                            className={styles.radioInput} // 숨기기 위해 스타일 클래스 추가
-                        />
-                        <img src={팥붕} alt="팥붕" className={styles.fishImage}/>
-                        <p className={styles.fishLabel}>팥붕</p>
-                    </label>
-                    <label className={styles.fishItem}>
-                        <input
-                            type="radio"
-                            name="choiceType"
-                            value="fish2"
-                            checked={formData.choiceType === "fish2"}
-                            onChange={handleChange}
-                            className={styles.radioInput}
-                        />
-                        <img src={슈붕} alt="슈붕" className={styles.fishImage}/>
-                        <p className={styles.fishLabel}>슈붕</p>
-                    </label>
-                    <label className={styles.fishItem}>
-                        <input
-                            type="radio"
-                            name="choiceType"
-                            value="fish3"
-                            checked={formData.choiceType === "fish3"}
-                            onChange={handleChange}
-                            className={styles.radioInput}
-                        />
-                        <img src={민초붕} alt="민초붕" className={styles.fishImage} />
-                        <p className={styles.fishLabel}>민초붕</p>
-                    </label>
-                    <label className={styles.fishItem}>
-                        <input
-                            type="radio"
-                            name="choiceType"
-                            value="fish4"
-                            checked={formData.choiceType === "fish4"}
-                            onChange={handleChange}
-                            className={styles.radioInput}
-                        />
-                        <img src={고구마붕} alt="고구마붕" className={styles.fishImage} />
-                        <p className={styles.fishLabel}>고구마붕</p>
-                    </label>
-                </div>
-
-                <h2 className={styles.sectionTitle}>쪽지 받고 싶은 주제를 설정하세요. (선택)</h2>
-                <div className={styles.noteBox}>
-                    <input
-                        type="text"
-                        name="topic"
-                        value={formData.topic}
-                        onChange={handleChange}
-                        placeholder="예) 두산 팬들만 쪽지 주세요!"
-                        className={styles.noteInput}
-                    />
-                </div>
-
-                <button type="submit" className={styles.createButton}>
-                    붕어빵 가게 생성
-                </button>
-            </form>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.inputContainer}>
+          <div className={styles.inputBox}>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className={styles.inputField}
+              placeholder="이름을 입력하세요."
+              required
+            />
+          </div>
+          <div className={styles.inputBox}>
+            <input
+              type="text"
+              name="studentID"
+              value={formData.studentID}
+              onChange={handleChange}
+              className={styles.inputField}
+              placeholder="학번을 입력하세요."
+              required
+            />
+          </div>
+          <div className={styles.inputBox}>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className={styles.inputField}
+              placeholder="비밀번호를 설정하세요."
+              required
+            />
+          </div>
+          <div className={styles.inputBox}>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              className={styles.inputField}
+              placeholder="다시 한번 입력하세요."
+              required
+            />
+          </div>
         </div>
-    );
+
+        <h2 className={styles.sectionTitle}>붕어빵을 선택하세요.</h2>
+        <div className={styles.fishSelection}>
+          <label className={styles.fishItem}>
+            <input
+              type="radio"
+              name="choiceType"
+              value="fish1"
+              checked={formData.choiceType === "fish1"}
+              onChange={handleChange}
+              className={styles.radioInput} // 숨기기 위해 스타일 클래스 추가
+            />
+            <img src={팥붕} alt="팥붕" className={styles.fishImage} />
+            <p className={styles.fishLabel}>팥붕</p>
+          </label>
+          <label className={styles.fishItem}>
+            <input
+              type="radio"
+              name="choiceType"
+              value="fish2"
+              checked={formData.choiceType === "fish2"}
+              onChange={handleChange}
+              className={styles.radioInput}
+            />
+            <img src={슈붕} alt="슈붕" className={styles.fishImage} />
+            <p className={styles.fishLabel}>슈붕</p>
+          </label>
+          <label className={styles.fishItem}>
+            <input
+              type="radio"
+              name="choiceType"
+              value="fish3"
+              checked={formData.choiceType === "fish3"}
+              onChange={handleChange}
+              className={styles.radioInput}
+            />
+            <img src={민초붕} alt="민초붕" className={styles.fishImage} />
+            <p className={styles.fishLabel}>민초붕</p>
+          </label>
+          <label className={styles.fishItem}>
+            <input
+              type="radio"
+              name="choiceType"
+              value="fish4"
+              checked={formData.choiceType === "fish4"}
+              onChange={handleChange}
+              className={styles.radioInput}
+            />
+            <img src={고구마붕} alt="고구마붕" className={styles.fishImage} />
+            <p className={styles.fishLabel}>고구마붕</p>
+          </label>
+        </div>
+
+        <h2 className={styles.sectionTitle}>
+          쪽지 받고 싶은 주제를 설정하세요. (선택)
+        </h2>
+        <div className={styles.noteBox}>
+          <input
+            type="text"
+            name="topic"
+            value={formData.topic}
+            onChange={handleChange}
+            placeholder="예) 두산 팬들만 쪽지 주세요!"
+            className={styles.noteInput}
+          />
+        </div>
+
+        <button type="submit" className={styles.createButton}>
+          붕어빵 가게 생성
+        </button>
+      </form>
+    </div>
+  );
 }
